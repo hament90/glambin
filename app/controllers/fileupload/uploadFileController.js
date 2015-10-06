58fd65ffb1982ba26f615263ce9f045f87c66689
Hament Aggarwal <fuchas_honey@yahoo.com>
2015-10-07 03:25:49 +0530

    file uploading

--- app/controllers/fileupload/uploadFileController.js
+++ app/controllers/fileupload/uploadFileController.js
@@ -6,7 +6,6 @@ var fileUploadController = new Controller();
 fileUploadController.mainProfile = function() {
 	var _nself=this;
     if (_nself.req.isAuthenticated() && _nself.req.files!=undefined && Object.keys(_nself.req.files).length>0 ){
-        console.log("lsaajdksajdlkklj ============== upload",_nself.req,"lsaajdksajdlkklj ============== upload=====================",_nself.req.user,"lsaajdksajdlkklj ============== upload=====================","")
         var obj={
             file:_nself.req.files.file,
             gbId:_nself.req.user.signUserId
@@ -23,7 +22,21 @@ fileUploadController.mainProfile = function() {
 }
 
 fileUploadController.uploadCover=function(){
-
+    var _nself=this;
+    if (_nself.req.isAuthenticated() && _nself.req.files!=undefined && Object.keys(_nself.req.files).length>0 ){
+        var obj={
+            file:_nself.req.files.file,
+            gbId:_nself.req.user.signUserId
+        };
+        var service= new uploadService();
+        service.on("done", function(status,msg,result,page){
+             console.log(result)
+            _nself.processJson(status,msg,result,page);
+        });
+        service.profilePicUploading(obj);
+    }else{
+         return _nself.res.redirect("/gb/404");
+    }
 }
 
 fileUploadController.uploadGallery=function(){
