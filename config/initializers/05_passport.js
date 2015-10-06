d25ac057c9d2befe03cb74f9c38a3fd4ed32b046
Hament Aggarwal <fuchas_honey@yahoo.com>
2015-10-07 02:13:14 +0530

    asdas

--- config/initializers/05_passport.js
+++ config/initializers/05_passport.js
@@ -60,7 +60,7 @@ passport.deserializeUser(function(id, done) {
     console.log("checkUser",user)
     if(user!=null){
       GBUserInfo.findOne({"gbId":user.gbId},{"handle":1,"gbId":1,"username":1,"shortDesc":1,"location":1,"profilePic":1,"profileCoverPic":1,"phone":1,"emailId":1,"signUserId":1},function(err,newUser){
-        console.log("newUser"newUser)
+        console.log("newUser",newUser)
         done(err, newUser);  
       });
     }else{
